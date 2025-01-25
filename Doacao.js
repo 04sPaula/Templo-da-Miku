d898cc258e58fed847097f006bee154f0104a3a7
Paula Alves <paula.edifba@gmail.com>
2025-01-25 14:07:23 -0300

    Criação da classe Doação

---
+++ Doacao.js
@@ -0,0 +1,69 @@
+class Doacao {
+    constructor(nomeDoador, valorDoacao, modoPagamento) {
+        this.idDoacao = null;
+        this.nomeDoador = nomeDoador;
+        this.valorDoacao = valorDoacao;
+        this.dataDoacao = new Date();
+        this.modoPagamento = modoPagamento;
+
+        if (this.valorDoacao <= 0) {
+            throw new Error('Valor da doação deve ser maior que zero');
+        }
+    }
+
+    static get MODOS_PAGAMENTO() {
+        return {
+            CARTAO: 'Cartao',
+            PIX: 'Pix',
+            ESPECIE: 'Especie',
+            BOLETO: 'Boleto'
+        };
+    }
+
+        // Getters
+        get idDoacao() {
+            return this._idDoacao;
+        }
+    
+        get nomeDoador() {
+            return this._nomeDoador;
+        }
+    
+        get valorDoacao() {
+            return this._valorDoacao;
+        }
+    
+        get dataDoacao() {
+            return this._dataDoacao;
+        }
+    
+        get modoPagamento() {
+            return this._modoPagamento;
+        }
+    
+        // Setters
+        set idDoacao(value) {
+            this._idDoacao = value;
+        }
+    
+        set nomeDoador(value) {
+            this._nomeDoador = value;
+        }
+    
+        set valorDoacao(value) {
+            if (value <= 0) {
+                throw new Error('Valor da doação deve ser maior que zero');
+            }
+            this._valorDoacao = value;
+        }
+    
+        set modoPagamento(value) {
+            const validModos = ['Cartao', 'Pix', 'Especie', 'Boleto'];
+            if (!validModos.includes(value)) {
+                throw new Error('Modo de pagamento inválido');
+            }
+            this._modoPagamento = value;
+        }
+}
+
+export default Doacao;
